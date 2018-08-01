e03decb4d892567205fb5be0147987788417df0c
Fabio Steffen <35061965+fdsteffen@users.noreply.github.com>
2018-08-01 17:41:10 +0200

    Add process_silentfile.sh

---
+++ bin/process_silentfile.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# removes everything behind NON_STANDARD_RESIDUE_MAP in the silent file which would otherwise break the script extract_lowscore_decoys.py 
+
+# cmd parsing functions
+usage() { echo "Remove non standard residues from silentfile 
+Usage: process_silentfile -s <silentfile>" 1>&2; exit 1; }
+
+
+#------------
+# cmd parsing
+#------------
+
+while getopts ":s:h" opt; do
+    case $opt in
+        s) 
+            silentfile=$OPTARG
+            ;;
+        h)
+            usage
+            ;;
+        \?)
+            invalidOpt
+            ;;
+        :)
+            missingArg
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+
+# no cmd line arguments given
+if [ -z "$silentfile" ]; then
+    usage
+fi
+
+# remove NON_STANDARD_RESIDUE tag line from silentfile (make a copy first)
+silentfile_proc=${silentfile%%.out}_proc.out
+#cp $silentfile $silentfile_proc
+cat $silentfile | awk -F '\NON_STANDARD_RESIDUE_MAP' '{ print $1 }' > $silentfile_proc
