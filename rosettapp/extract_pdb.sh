12ca800dcd45ccec9ca1529e9373e9c81423e1a0
Fabio Steffen <35061965+fdsteffen@users.noreply.github.com>
2020-12-04 13:28:52 +0100

    Add option to merge pdbs into single file

--- rosettapp/extract_pdb.sh
+++ rosettapp/extract_pdb.sh
@@ -2,7 +2,7 @@
 
 # cmd parsing functions
 usage() { echo "Extract n pdbs with the lowest energy score from silentfile 
-Usage: extract_pdb.sh -s <silentfile> -f <folder with silentfiles> -n <number of models> -e <extract pdbs (true|false, default: true)>"1>&2; exit 1; }
+Usage: extract_pdb.sh -s <silentfile> -f <folder with silentfiles> -n <number of models> -e <extract pdbs (true|false, default: true)> -m <merge pdbs (true|false, defauöt: false)>"1>&2; exit 1; }
 invalidOpt() { echo "Invalid option: -$OPTARG" 1>&2; exit 1; }
 missingArg() { echo "Option -$OPTARG requires an argument" 1>&2; exit 1; }
 
@@ -10,7 +10,7 @@ missingArg() { echo "Option -$OPTARG requires an argument" 1>&2; exit 1; }
 # cmd parsing
 #------------
 
-while getopts ":s:f:n:e:h" opt; do
+while getopts ":s:f:n:e:m:h" opt; do
     case $opt in
         s) 
             silentfile=$OPTARG
@@ -23,6 +23,9 @@ while getopts ":s:f:n:e:h" opt; do
             ;;
         e)
             extract=$OPTARG
+            ;;
+        m)
+            merge=$OPTARG
             ;;
         h)
             usage
@@ -78,9 +81,26 @@ fi
 grep "^SCORE:" "$silentfile" | grep -v description | sort -nk2 | head -n "$number" | awk '{print $NF ": " $2}' | tee tmp_models
 echo ""
 
-# check if file is present
+# extract pdbs or do not
+extract=`echo "$extract" | tr [:upper:] [:lower:]`
 if [ "$extract" != "false" ]; then
-    extract_pdbs.linuxgccrelease -in:file:silent "$silentfile" -tags `cat tmp_models | cut -f1 -d':'` && echo "$(wc -l <tmp_models) model(s) successfully extracted!" && rm tmp_models
-else
-    rm tmp_models
+    extract_pdbs.linuxgccrelease -in:file:silent "$silentfile" -tags `cat tmp_models | cut -f1 -d':'` && echo "$(wc -l <tmp_models) model(s) successfully extracted!"
 fi
+
+# merge pdb into single file or not
+merge=`echo "$merge" | tr [:upper:] [:lower:]`
+if [ "$merge" = "true" ]; then
+    i=1
+    pdb_merge=`echo ${silentfile%.*}.pdb`
+    > $pdb_merge
+    for pdb in `cat tmp_models | cut -f1 -d':'`; do
+        echo MODEL $i >> "$pdb_merge" 
+        cat "$pdb".pdb >> "$pdb_merge"
+        echo ENDMDL >> "$pdb_merge"
+        echo;echo >> "$pdb_merge"
+        rm "$pdb".pdb
+        i=$(($i+1))
+    done
+fi
+
+rm tmp_models
